fc8df6116982c78aba7006e37c4ee2b0314071cd
RuihaoYe <ruihaoye@163.com>
2015-01-03 15:33:21 +0800

    add htmlize.el

--- init/init-org.el
+++ init/init-org.el
@@ -1,4 +1,16 @@
+(require 'htmlize)
+
+(add-hook 'org-mode-hook
+          'linum-mode)
+
 (setq org-todo-keywords
       '((sequence "TODO" "DOING" "|" "DONE" "CANCELED")))
+
+(add-hook 'org-mode-hook
+          '(lambda ()
+             (local-set-key (kbd "M-a") 'move-beginning-of-line)))
+(add-hook 'org-mode-hook
+          '(lambda ()
+             (local-set-key (kbd "M-e") 'move-end-of-line)))
 
 (provide 'init-org)
